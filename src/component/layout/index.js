43566387f9d0f557dd0f4e19f0f85bb5492d2cba
hetong <1061459128@qq.com>
2023-09-27 11:20:15 +0800

    ~

--- src/component/layout/index.js
+++ src/component/layout/index.js
@@ -2,6 +2,7 @@ import '@/app/globals.css';
 import '../../style/layout.css';
 import { Menu, ConfigProvider, Button } from 'antd';
 import { ThunderboltFilled  } from '@ant-design/icons';
+import Head from 'next/head';
 
 import react, { Fragment, useEffect, useState } from 'react';
 import Link from 'next/link';
@@ -151,40 +152,48 @@ export default function RootLayout({ children }) {
     }).play();
   }
   return (
-    <div style={{
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      background: '#000'
-    }}>
-      <div className='menu_content'>
-        <MenuList onSelect={openMenu} />
-      </div>
-      <div className='main_content'>
-        <span className='main_icon'>
-          HeT.
-        </span>
-        {children}
-      </div>
-      <div
-        onClick={openMenu}
-        className='menu_btn'
-      >
-        <svg
-          className='menu_icon'
-          t="1695259926167"
-          viewBox="0 0 1024 1024"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          p-id="5380" id="mx_n_1695259926168"
-          width="48" height="48">
-          <path
-            d="M473.744 541.92a16 16 0 0 1 16 16v249.728a16 16 0 0 1-16 16H224a16 16 0 0 1-16-16V557.92a16 16 0 0 1 16-16h249.744z m322.784 0a16 16 0 0 1 16 16v249.728a16 16 0 0 1-16 16h-249.76a16 16 0 0 1-16-16V557.92a16 16 0 0 1 16-16h249.76z m-354.784 48H256v185.728h185.744V589.92z m322.784 0h-185.76v185.728h185.76V589.92zM676.528 208a152 152 0 1 1 0 304 152 152 0 0 1 0-304z m-202.784 11.136a16 16 0 0 1 16 16v249.728a16 16 0 0 1-16 16H224a16 16 0 0 1-16-16V235.136a16 16 0 0 1 16-16h249.744zM676.528 256a104 104 0 1 0 0 208 104 104 0 0 0 0-208z m-234.784 11.136H256v185.728h185.744V267.136z"
-            fill={isMenuon ? "#C4853C" : "#453BC3"}
-            p-id="5381"
+    <div>
+      <Head>
+        <title>HeT.</title>
+        <link rel="icon" href="/favicon.ico" /> {/* 设置标签页图标 */}
+      </Head>
+      <div>
+        <div style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          background: '#000'
+        }}>
+          <div className='menu_content'>
+            <MenuList onSelect={openMenu} />
+          </div>
+          <div className='main_content'>
+            <span className='main_icon'>
+              HeT.
+            </span>
+            {children}
+          </div>
+          <div
+            onClick={openMenu}
+            className='menu_btn'
           >
-          </path>
-        </svg>
+            <svg
+              className='menu_icon'
+              t="1695259926167"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="5380" id="mx_n_1695259926168"
+              width="48" height="48">
+              <path
+                d="M473.744 541.92a16 16 0 0 1 16 16v249.728a16 16 0 0 1-16 16H224a16 16 0 0 1-16-16V557.92a16 16 0 0 1 16-16h249.744z m322.784 0a16 16 0 0 1 16 16v249.728a16 16 0 0 1-16 16h-249.76a16 16 0 0 1-16-16V557.92a16 16 0 0 1 16-16h249.76z m-354.784 48H256v185.728h185.744V589.92z m322.784 0h-185.76v185.728h185.76V589.92zM676.528 208a152 152 0 1 1 0 304 152 152 0 0 1 0-304z m-202.784 11.136a16 16 0 0 1 16 16v249.728a16 16 0 0 1-16 16H224a16 16 0 0 1-16-16V235.136a16 16 0 0 1 16-16h249.744zM676.528 256a104 104 0 1 0 0 208 104 104 0 0 0 0-208z m-234.784 11.136H256v185.728h185.744V267.136z"
+                fill={isMenuon ? "#C4853C" : "#453BC3"}
+                p-id="5381"
+              >
+              </path>
+            </svg>
+          </div>
+        </div>
       </div>
     </div>
   )

6ea8850608d52be5c17dc676728466c1c7c59df7
hetong <1061459128@qq.com>
2023-10-08 17:26:41 +0800

    20231008 更新

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,14 +6,27 @@ export default function About() {
   const [imgList, setImgList] = useState([10, 12, 15, 19, 20])
   const [about_img_top_height, setabout_img_top_height] = useState(940 + 283);
   const [about_img_bottmo_height, setabout_img_bottom_height] = useState(940 + 400);
+  const [about_img_list_bottmo_height, setabout_img_list_bottmo_height] = useState(940);
+  const [bottom_list_one_width , setbottom_list_one_width] = useState(0);
+  const [isTextShow, setIsTextShow] = useState(false);
   useEffect(() => {
     const aboutImgTop = document.getElementById('about_img_top');
     const aboutImgBottom = document.getElementById('about_img_bottom');
     const aboutImgBottomText = document.getElementById('about_img_bottom_text');
     const aboutImgBottomList = document.getElementById('about_img_bottom_list');
     const imgTopListElementes = document.querySelectorAll('.about_img_top_item');
+    const bottomImageStartTime = window.innerHeight * 0.9;
+    const img_bottom_mask = document.getElementById('img_bottom_mask');
+    const bottom_list_one = document.getElementById('bottom_list_one');
+    const bottom_list_two = document.getElementById('bottom_list_two');
+    const bottom_list_three = document.getElementById('bottom_list_three');
+    // console.log(bottom_list_one.width)
+    // setbottom_list_one_width(bottom_list_one.width);
+    bottom_list_two.style.transform = `translateX(${bottom_list_one.width * 2 + 20}px) translateY(105%)`;
+    bottom_list_three.style.transform = `translateX(${bottom_list_one.width * 2 + 20}px) translateY(0)`;
     setabout_img_top_height(window.innerHeight + 283);
     setabout_img_bottom_height(window.innerHeight + 400);
+    setabout_img_list_bottmo_height(window.innerHeight * 0.9);
     document.addEventListener('wheel', function(e) {
       let aboutImgToptop = aboutImgTop.getBoundingClientRect().top;
       if (aboutImgToptop <= 0 && aboutImgToptop >= -238) {
@@ -22,30 +35,75 @@ export default function About() {
         })
       }
       let aboutImgBottomtop = aboutImgBottom.getBoundingClientRect().top;
-      if (aboutImgBottomtop <= 200 && aboutImgBottomtop >= 0) {
-        const p = 200 - aboutImgBottomtop;
-        aboutImgBottomText.style.display = 'block';
-        aboutImgBottomText.style.opacity = p * 0.005 + '';
-        aboutImgBottomText.style.top = 500 - p + 'px';
-        aboutImgBottomList.style.opacity = p * 0.005 + '';
+      
+      if (aboutImgBottomtop < bottomImageStartTime) {
+        const c = bottomImageStartTime - aboutImgBottomtop;
+        // console.log(aboutImgBottomtop);
+        if (aboutImgBottomtop < bottomImageStartTime && aboutImgBottomtop >= 0) {
+          img_bottom_mask.style.height = aboutImgBottomtop + 'px'
+          img_bottom_mask.style.width = '100%'
+        }
+        if (aboutImgBottomtop <= 0) {
+          img_bottom_mask.style.height = 0;
+        }
+        if (aboutImgBottomtop <= 0 && aboutImgBottomtop >= -500) {
+          bottom_list_one.style.transform = `translateY(${50 + aboutImgBottomtop/10}%) translateX(95%) scale(${2 + aboutImgBottomtop * 0.002 })`
+          bottom_list_two.style.transform = `translateY(${100 + aboutImgBottomtop/5}%) translateX(${bottom_list_one.width * (2 + aboutImgBottomtop * 0.002) + 20}px)`
+          bottom_list_two.style.opacity = -aboutImgBottomtop/500;
+          setIsTextShow(e.deltaY >= 0);
+          bottom_list_three.style.display = 'none';
+        }
+        if (aboutImgBottomtop < -500) {
+          bottom_list_one.style.transform = `translateY(0) translateX(95%) scale(1)`
+          bottom_list_two.style.transform = `translateY(0) translateX(${bottom_list_one.width + 20}px)`
+          bottom_list_two.style.opacity = '1';
+          bottom_list_three.style.display = 'inline';
+        }
+        if (aboutImgBottomtop <= -500 && aboutImgBottomtop >= -(500 + bottom_list_one.width * 0.95 - 20)) {
+          const x = -500 - aboutImgBottomtop;
+          bottom_list_one.style.transform = `translateY(0) translateX(${bottom_list_one.width * 0.95 - x}px) scale(1)`
+          bottom_list_two.style.transform = `translateY(0) translateX(${bottom_list_one.width * 0.95 - x + 20}px) scale(1)`
+          bottom_list_three.style.transform = `translateY(0) translateX(${bottom_list_one.width * 0.95 - x + 40}px) scale(1)`
+        }
+        if (aboutImgBottomtop <= -(500 + bottom_list_one.width * 0.95 - 20)) {
+          bottom_list_one.style.transform = `translateY(0) translateX(20px) scale(1)`
+          bottom_list_two.style.transform = `translateY(0) translateX(40px) scale(1)`
+          bottom_list_three.style.transform = `translateY(0) translateX(60px) scale(1)`
+        }
       }
-      if (aboutImgBottomtop >= 200) {
-        aboutImgBottomText.style.display = 'none';
-        aboutImgBottomList.style.opacity = '0';
-      }
-      if (aboutImgBottomtop < 0 && aboutImgBottomtop > -200 ) {
-        const p = 200 + aboutImgBottomtop;
-        aboutImgBottomText.style.opacity = p * 0.005 + '';
-        aboutImgBottomText.style.display = 'block';
-        aboutImgBottomList.style.transform = `translateX(${p}px)`;
-      }
-      if (aboutImgBottomtop <= -200 ) {
-        aboutImgBottomText.style.display = 'none';
-        aboutImgBottomList.style.opacity = 1;
-        aboutImgBottomList.style.transform = `translateX(${0}px)`;
-      }
+      // if (aboutImgBottomtop <= 200 && aboutImgBottomtop >= 0) {
+      //   const p = 200 - aboutImgBottomtop;
+      //   aboutImgBottomText.style.display = 'block';
+      //   aboutImgBottomText.style.opacity = p * 0.005 + '';
+      //   aboutImgBottomText.style.top = 500 - p + 'px';
+      //   aboutImgBottomList.style.opacity = p * 0.005 + '';
+      // }
+      // if (aboutImgBottomtop >= 200) {
+      //   aboutImgBottomText.style.display = 'none';
+      //   aboutImgBottomList.style.opacity = '0';
+      // }
+      // if (aboutImgBottomtop < 0 && aboutImgBottomtop > -200 ) {
+      //   const p = 200 + aboutImgBottomtop;
+      //   aboutImgBottomText.style.opacity = p * 0.005 + '';
+      //   aboutImgBottomText.style.display = 'block';
+      //   aboutImgBottomList.style.transform = `translateX(${p}px)`;
+      // }
+      // if (aboutImgBottomtop <= -200 ) {
+      //   aboutImgBottomText.style.display = 'none';
+      //   aboutImgBottomList.style.opacity = 1;
+      //   aboutImgBottomList.style.transform = `translateX(${0}px)`;
+      // }
     });
   }, []);
+  useEffect(() => {
+    anime({
+      targets: '#about_img_bottom_text',
+      translateY: isTextShow ? 0 : 200,
+      opacity: isTextShow ? 1 : 0,
+      duration: 1000,
+      easing: 'easeInOutQuad'
+    }).play();
+  }, [isTextShow])
   return (
     <div style={{
       width: '100%',
@@ -76,16 +134,15 @@ export default function About() {
         May your Majesty entrust the minister to the effect of the revival of the thief, if not, the sin of the minister, to the spirit of the emperor. If there is no word of Xingde, then blame Yu, Yi, allow and so on slow, in order to clear its fault; Your Majesty should also help yourself, to Suwa good way, to look into the wisdom of the words, to follow the legacy of the emperor, I am indebted to you.
         Today, when away, face tears, do not know what to say.
       </div>
-      <div id="about_img_bottom" style={{height: about_img_bottmo_height + 'px', position: 'relative'}}>
-        <div id="about_img_bottom_text" className="about_img_bottom_text about-text">
-          "Le Chuang No Boundaries" is a new creative platform launched by [Coca-Cola] worldwide, which gives the iconic [Coca-Cola] a new expression and meaning through limited product launch, immersive interactive experience and cultural creativity in line with the younger generation. "The Next 3000 Years" is the fifth limited product launched by the "Le Chuang No Boundaries" creative platform of [Coca-Cola] in the Chinese market after "Star Walk", "Rhythm Square", "Soul · Environment" and "Hero Debut".
-        </div>
-        <div id="about_img_bottom_list" className="about_img_bottom_list">
-          {[26, 27, 28, 20].map(m => {
-            return (
-              <img key={m} src={`/img/${m}.jpg`} className="about_img_bottom_item" style={{height: '100%', marginRight: '20px'}} />
-            )
-          })}
+      <div id="about_img_bottom" style={{height: 3000 + 'px', position: 'relative'}}>
+      <div className="about_img_bottom_list" style={{height: about_img_list_bottmo_height + 'px' }}>
+          <div id="img_bottom_mask"></div>
+          <img id="bottom_list_one" src="/img/1.jpg"></img>
+          <img id="bottom_list_two" src="/img/15.jpg"></img>
+          <img id="bottom_list_three" src="/img/18.jpg"></img>
+          <div id="about_img_bottom_text">
+            You do not see the water of the Yellow River coming from the sky, rushing to the sea and never coming back
+          </div>
         </div>
       </div>
     </div>
